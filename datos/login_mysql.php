1c5187782729b5fc011d2097423a91eda9045ef4
R0bert0Vazquez <l21390340@chetumal.tecnm.mx>
2025-05-22 14:33:59 -0500

    Add files via upload

---
+++ datos/login_mysql.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Provee las constantes para conectar a la base de datos Mysql.
+ */
+define("NOMBRE_HOST", "localhost");
+define("BASE_DE_DATOS", "controlasistenciaalumnos");
+define("USUARIO", "root");
+define("CONTRASENA", "");
+?>
