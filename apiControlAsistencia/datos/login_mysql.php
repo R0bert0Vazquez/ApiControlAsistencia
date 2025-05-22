164fb6bb336b021fba301bb8980740b4b1ef95d2
R0bert0Vazquez <l21390340@chetumal.tecnm.mx>
2025-05-22 14:33:27 -0500

    Delete apiControlAsistencia directory

--- apiControlAsistencia/datos/login_mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Provee las constantes para conectar a la base de datos Mysql.
- */
-define("NOMBRE_HOST", "localhost");
-define("BASE_DE_DATOS", "controlasistenciaalumnos");
-define("USUARIO", "root");
-define("CONTRASENA", "");
-?>
